4cc72c1e94f4e1f1df78d4cdc1bd2e8f7b805b76
sunny <sunny@riddimsoftware.com>
2026-01-21 13:39:17 -0500

    fix artist search

--- ios/App/JustPlayIt/PlayerView/PlayerView.swift
+++ ios/App/JustPlayIt/PlayerView/PlayerView.swift
@@ -142,8 +142,12 @@ struct PlayerView: View {
 									saveTrack(track)
 								}
 								let allResults = results ?? []
-								self.searchResults = allResults.filter { track in
-									allResults.contains { $0.title.lowercased() == track.title.lowercased() && $0.artist.lowercased() != track.artist.lowercased() }
+								if title.isEmpty && !artist.isEmpty {
+									self.searchResults = allResults
+								} else {
+									self.searchResults = allResults.filter { track in
+										allResults.contains { $0.title.lowercased() == track.title.lowercased() && $0.artist.lowercased() != track.artist.lowercased() }
+									}
 								}
 								self.isSearching = false
 							}
