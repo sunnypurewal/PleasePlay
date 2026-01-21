52901682ce33742e7ee91df59a372107c7991fb9
sunny <sunny@riddimsoftware.com>
2026-01-21 14:21:46 -0500

    add MusicRecognition

---
+++ ios/MusicRecognition/Sources/MusicRecognition/MusicRecognition.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+public struct MusicRecognitionResult: Sendable, Equatable {
+    public let title: String
+    public let artist: String
+    public let album: String?
+    public let artworkUrl: URL?
+    public let storefrontId: String?
+    public let recognizedAt: Date
+
+    public init(
+        title: String,
+        artist: String,
+        album: String?,
+        artworkUrl: URL?,
+        storefrontId: String?,
+        recognizedAt: Date = Date()
+    ) {
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.artworkUrl = artworkUrl
+        self.storefrontId = storefrontId
+        self.recognizedAt = recognizedAt
+    }
+}
+
+public protocol MusicRecognitionProtocol {
+    func recognizeSingleSong() async throws -> MusicRecognitionResult
+
+    func startContinuousRecognition(
+        for duration: TimeInterval?,
+        onRecognition: @escaping @Sendable (MusicRecognitionResult) -> Void
+    ) async throws
+
+    func stopContinuousRecognition() async
+
+}
